07e089b92106d475636a4f7b308b40267395cde1
Scott White <whiteau@fhcrc.org>
2018-01-03 15:19:57 -0800

    Initial updates of problem 2 formulation

--- problem_2.py
+++ problem_2.py
@@ -1,53 +1,79 @@
-
 # coding: utf-8
 
-# In[ ]:
-
-import pandas as pd
+import argparse
+import configparser
+import logging
 import os
-from tkinter import filedialog
-from tkinter import *
+
+from utilities.execution_modes import ExecutionType
+import utilities.fileutils as fileutils
 
 
-# In[ ]:
 
-#Used to set up and open the file path.
-def openfiles():
-    root = Tk()
-    files = filedialog.askopenfilenames(parent=root, title='Select a file and click open')
-    filepath = root.tk.splitlist(files)
-    return filepath
+formatter = logging.Formatter(
+    "%(asctime)s %(threadName)-11s %(levelname)-10s %(message)s")
 
+logger = logging.getLogger()
+streamhandler = logging.StreamHandler()
+streamhandler.setLevel(logging.INFO)
+streamhandler.setFormatter(formatter)
+logger.addHandler(streamhandler)
 
-# In[ ]:
+logger.setLevel(logging.DEBUG)
 
-def combinedlist(pattern, datalen, exportfile, sequenceColumn, addColumn):
-    #calling the first function
-    filegroup = openfiles()
-    matchlist = []
-    #Reading in the chosen csv file, selecting necessary columns, and adding it to a list
-    for file in filegroup:
-        new_df = pd.read_csv(file)
-        new_df["File Name"] = os.path.split(file)[1]
-        new_df = new_df[["File Name", sequenceColumn, addColumn]]
-        matchlist.append(new_df)
-    #Return peptide sequences that doesn't match the regex pattern
-    for df1 in matchlist:
-        df1 = df1.loc[0:datalen]
-    final_df = df1[~df1.Sequence.str.contains(pattern)]
-    final_df.to_csv(exportfile, encoding='utf-8', index=False)
-    return final_df
+EXECUTION_TYPE_TO_LOGGER_LEVEL = {
+    ExecutionType.TEST : logging.DEBUG,
+    ExecutionType.PRODUCTION : logging.DEBUG, #change to INFO when done
+    }
+
+config = configparser.ConfigParser()
+configFilePath = 'config'
+config.read(configFilePath)
 
 
-# In[ ]:
+cmd_arg_parser = argparse.ArgumentParser("problem_2: screens a given directories' CSV files for compliance with a given regular expression")
+cmd_arg_parser.add_argument('-t', action="store_true", default=False,
+                            dest="is_test",
+                            help="Set mode to test. There will be additional debug information and no computationally expensive functions will be run.")
+cmd_arg_parser.add_argument('--folder',
+                            action="store",
+                            dest="file_dir",
+                            help="Load CSV's from supplied directory.  by default this defaults to {}".format(config['DEFAULT']['input']))
+cmd_arg_parser.add_argument('--output',
+                            action="store",
+                            dest="output_dir",
+                            help="Target file and location to write results out to. by default this defaults to {}".format(config['DEFAULT']['output']))
+
+
+cmd_args = cmd_arg_parser.parse_args()
+
+if cmd_args.is_test == True:
+    exe_mode = ExecutionType.TEST
+else:
+    exe_mode = ExecutionType.PRODUCTION
+
+logger.setLevel(EXECUTION_TYPE_TO_LOGGER_LEVEL[exe_mode])
+
+file_directory = config['DEFAULT']['input']
+if cmd_args.file_dir != None:
+    file_directory = cmd_args.file_dir
+output_path = config['DEFAULT']['output']
+if cmd_args.output_dir != None:
+    output_path= cmd_args.output_dir
+
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+
 
 #(regex pattern, number of rows with data minus 2, name of the peptide sequence column, name of the protein group accessions column)
 #pattern for HLA-A11 is r'\b.[VIFY][MLFYIA]\w+[LIYVF].[KR]\b'
 #pattern for HLA-A24 is r'\b.[YF]\w+[LFI]\b'
-combinedlist(r'\b.[VIFY][MLFYIA]\w+[LIYVF].[KR]\b', 2565, "C:/Users/Sujatha/Desktop/example 2.csv", "Sequence", "Protein Group Accessions")
+sieved_dataframes = fileutils.combinedlist(r'\b.[VIFY][MLFYIA]\w+[LIYVF].[KR]\b',  "Sequence", "Protein Group Accessions")
+logger.info("returned dataframes were: {}".format(sieved_dataframes))
 
-
-# In[ ]:
+if exe_mode == ExecutionType.PRODUCTION:
+    logger.info("writing output to {}".format(output_path))
+    fileutils.write_sieved_dataframe_dict_to_csv(sieved_dataframes, output_path, "problem_2_example")
 
 
 
